49054012db62b3d5e3bf970cb8e52b4459f277c4
Mutesi <ktesialine@gmail.com>
2023-02-07 23:52:38 +0200

    feat: card profile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Button from "./components/ui/Button";
+import React from "react";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-black w-[550px] max-w-[317px]  mt-60  mx-60 rounded-t-[10px]">
+      <div className="max-w-[317px] mx-auto">
+        <img
+          src="/images/Rectangle 90.png"
+          alt="profile"
+          className="w-fit object-cover  "
+        />
+        <div className="flex justify-center gap-4 py-6">
+          <Button icon="/icons/Mail.svg">Email</Button>
+          <Button icon="/icons/linkedin.svg" styles="bg-blue-500 text-white">
+            Linkedin
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/ui/Button.js
@@ -0,0 +1,11 @@
+import React from "react"
+export default function Button({children, icon ,styles}){
+  return (
+    <button
+      className={`text-black px-4 py-2 rounded-md bg-white flex gap-2 items-center ${styles}`}
+    >
+      <img src={icon} alt="icon" className="w-8" />
+      <p>{children}</p>
+    </button>
+  );
+}
